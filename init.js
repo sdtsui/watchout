b194ab71f29f7e592c2359fde52386f5edb9240b
sdtsui <szehungdanieltsui@gmail.com>
2014-12-19 16:32:44 -0800

    Enemies move to a new random location.

--- init.js
+++ init.js
@@ -1,5 +1,11 @@
 var svg = document.getElementById("gameboard");
 gameBoardWidth = +svg.getAttribute("width");
 gameBoardHeight = +svg.getAttribute("height");
-var numEnemies = 5;
+var numEnemies = 20;
+var enemies = [];
 createEnemies(numEnemies);
+
+// Create interval to move enemies to a random location every second.
+var gameTimer = setInterval(function(){
+  moveEnemiesToRandomLocation();
+} , 1000);
--- watchout.js
+++ watchout.js
@@ -6,30 +6,32 @@ var Flotsam = function(){
 
 
 var Enemy = function(x,y, imgURL){
+  this.height = 50;
+  this.width = 50;
 
   if (x===undefined){
-    x = Math.random()*gameBoardWidth;
+    x = getRandomInt(0, gameBoardWidth-this.width);
   }
   if (y===undefined){
-    y = Math.random()*gameBoardHeight;
+    y = getRandomInt(0, gameBoardHeight-this.height);
   }
 
   this.x = x;
   this.y = y;
   this.imgURL = imgURL;
-  this.height = "50px";
-  this.width = "50px";
 }
 
 Enemy.prototype = Object.create(Flotsam.prototype);
 Enemy.prototype.constructor = Enemy;
+Enemy.prototype.move = function(){
+  this.x = getRandomInt(0, gameBoardWidth-this.width);
+  this.y = getRandomInt(0, gameBoardHeight-this.height);
+}
 
 
 var Player = function(){}
 
 var createEnemies = function (num){
-  var enemies = [];
-
   for (var i=0; i<num; i++){
     var enemy = new Enemy( undefined, undefined ,"asteroid.png");
     enemies.push(enemy);
@@ -39,11 +41,30 @@ var createEnemies = function (num){
     .append("image")//.attr("xlink:href", function(d){return d.imgURL;})
     .attr(
       {
+        "class" : "enemy",
         "xlink:href": function(d){return d.imgURL;},
-        "x": function(d){return d.x;},
-        "y": function(d){return d.y;},
+        "x": function(d){return d.x+"px";},
+        "y": function(d){return d.y+"px";},
         "height": function(d){return d.height},
         "width":function(d){return d.width}
       });
 };
 
+var moveEnemiesToRandomLocation = function (){
+  enemies.forEach(function(enemy){
+    enemy.move();
+  });
+  d3.selectAll("image.enemy").data(enemies)
+    .transition()
+    .duration(1000)
+    .attr(
+      {
+        "x": function(d){return d.x+"px";},
+        "y": function(d){return d.y+"px";},
+      });
+}
+
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
